ee42b204565441302192035274850478db0a7568
Matteo Sovilla <teo.sovi@gmail.com>
2018-01-04 16:24:43 +0100

    Icona loading aggiunta
    Signed-off-by: Matteo Sovilla <teo.sovi@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'main_menu.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 import 'drawer.dart';
+import 'main_menu.dart';
+
+final googleSignIn = new GoogleSignIn();
 
 void main() {
   runApp(new MaterialApp(
@@ -19,18 +25,31 @@ void main() {
 
 class MyPage extends StatelessWidget {
   MyPage({this.title});
+
   final String title;
+
+  Future<Null> _ensureLoggedIn() async {
+    GoogleSignInAccount user = googleSignIn.currentUser;
+    if (user == null)
+      user = await googleSignIn.signInSilently();
+    if (user == null) {
+      await googleSignIn.signIn();
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text("Setnote - "+title)
+            title: new Text("Setnote - " + title)
         ),
         drawer: new Drawer(
           child: new MyDrawer(),
         ),
-        body: new Text(title),
+        body: new Center(
+          child: new CircularProgressIndicator(),
+        )
     );
   }
 }
