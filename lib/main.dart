30a1f8c2419e884d9c7ea34ff8f8566459aeb8c9
Matteo Sovilla <teo.sovi@gmail.com>
2018-01-05 11:41:01 +0100

    riorganizzati file con constants pt2
    Signed-off-by: Matteo Sovilla <teo.sovi@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart' as constant;
 import 'main_menu.dart';
 import 'mypage.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: "Setnote",
+    title: app_name,
     home: new MainMenu(), // becomes the route named '/'
     routes: <String, WidgetBuilder>{
-      '/match': (BuildContext context) => new MyPage(title: 'match'),
-      '/team': (BuildContext context) => new MyPage(title: 'team'),
-      '/stats': (BuildContext context) => new MyPage(title: 'stats'),
-      '/history': (BuildContext context) => new MyPage(title: 'history'),
-      '/settings': (BuildContext context) => new MyPage(title: 'settings'),
+      '/match': (BuildContext context) =>
+      new MyPage(title: constant.match_label),
+      '/team': (BuildContext context) => new MyPage(title: constant.team_label),
+      '/stats': (BuildContext context) =>
+      new MyPage(title: constant.stats_label),
+      '/history': (BuildContext context) =>
+      new MyPage(title: constant.history_label),
+      '/settings': (BuildContext context) =>
+      new MyPage(title: constant.settings_label),
     },
   ));
 }
--- lib/mypage.dart
+++ lib/mypage.dart
@@ -3,9 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'constants.dart' as constant;
 import 'drawer.dart';
-
-final googleSignIn = new GoogleSignIn();
 
 class MyPage extends StatelessWidget {
   MyPage({this.title});
@@ -13,17 +12,17 @@ class MyPage extends StatelessWidget {
   final String title;
 
   Future<Null> _ensureLoggedIn() async {
-    GoogleSignInAccount user = googleSignIn.currentUser;
-    if (user == null) user = await googleSignIn.signInSilently();
+    GoogleSignInAccount user = constant.googleSignIn.currentUser;
+    if (user == null) user = await constant.googleSignIn.signInSilently();
     if (user == null) {
-      await googleSignIn.signIn();
+      await constant.googleSignIn.signIn();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(title: new Text("Setnote - " + title)),
+        appBar: new AppBar(title: new Text(constant.app_name + " - " + title)),
         drawer: new Drawer(
           child: new MyDrawer(),
         ),
