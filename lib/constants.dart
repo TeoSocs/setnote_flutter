f4740cdb7dd28af3dd5b859dd619359f4279f600
Matteo Sovilla <teo.sovi@gmail.com>
2018-01-05 11:24:58 +0100

    riorganizzati file con constants
    Signed-off-by: Matteo Sovilla <teo.sovi@gmail.com>

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+const match_label = "Nuova partita";
+const team_label = "Gestione squadra";
+const stats_label = "Statistiche squadra";
+const history_label = "Archivio partite";
+const settings_label = "Impostazioni";
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
-import 'drawer.dart';
 import 'main_menu.dart';
-
-final googleSignIn = new GoogleSignIn();
+import 'mypage.dart';
 
 void main() {
   runApp(new MaterialApp(
     title: "Setnote",
     home: new MainMenu(), // becomes the route named '/'
-    routes: <String, WidgetBuilder> {
+    routes: <String, WidgetBuilder>{
       '/match': (BuildContext context) => new MyPage(title: 'match'),
       '/team': (BuildContext context) => new MyPage(title: 'team'),
       '/stats': (BuildContext context) => new MyPage(title: 'stats'),
@@ -20,36 +15,4 @@ void main() {
       '/settings': (BuildContext context) => new MyPage(title: 'settings'),
     },
   ));
-}
-
-
-class MyPage extends StatelessWidget {
-  MyPage({this.title});
-
-  final String title;
-
-  Future<Null> _ensureLoggedIn() async {
-    GoogleSignInAccount user = googleSignIn.currentUser;
-    if (user == null)
-      user = await googleSignIn.signInSilently();
-    if (user == null) {
-      await googleSignIn.signIn();
-    }
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-            title: new Text("Setnote - " + title)
-        ),
-        drawer: new Drawer(
-          child: new MyDrawer(),
-        ),
-        body: new Center(
-          child: new CircularProgressIndicator(),
-        )
-    );
-  }
 }
