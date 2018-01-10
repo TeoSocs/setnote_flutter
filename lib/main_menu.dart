30ab9321945101f3b4a0330265153bd2c3700b16
ICNT\YYE5730 <teo.sovi@gmail.com>
2018-01-10 13:35:53 +0100

    cambiato aspetto main menu

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -12,25 +12,49 @@ class MainMenu extends StatelessWidget {
         child: new ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: 420.0),
           child: new ListView(
-            shrinkWrap: true,
-            padding: constant.standard_margin,
             children: <Widget>[
-              new SetnoteButton(
-                  label: constant.match_label,
-                  onPressed: () => Navigator.of(context).pushNamed("/match")),
-              new SetnoteButton(
-                  label: constant.team_label,
-                  onPressed: () => Navigator.of(context).pushNamed("/team")),
-              new SetnoteButton(
-                  label: constant.stats_label,
-                  onPressed: () => Navigator.of(context).pushNamed("/stats")),
-              new SetnoteButton(
-                  label: constant.history_label,
-                  onPressed: () => Navigator.of(context).pushNamed("/history")),
-              new SetnoteButton(
-                  label: constant.settings_label,
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed("/settings")),
+              new Card(
+                child: new FlatButton(
+                    child: new ListTile(
+                      leading: const Icon(Icons.add),
+                      title: const Text(constant.match_label),
+                    ),
+                    onPressed: () => Navigator.of(context).pushNamed("/match")),
+              ),
+              new Card(
+                child: new FlatButton(
+                    child: new ListTile(
+                      leading: const Icon(Icons.contacts),
+                      title: const Text(constant.team_label),
+                    ),
+                    onPressed: () => Navigator.of(context).pushNamed("/team")),
+              ),
+              new Card(
+                child: new FlatButton(
+                    child: new ListTile(
+                      leading: const Icon(Icons.assessment),
+                      title: const Text(constant.stats_label),
+                    ),
+                    onPressed: () => Navigator.of(context).pushNamed("/stats")),
+              ),
+              new Card(
+                child: new FlatButton(
+                    child: new ListTile(
+                      leading: const Icon(Icons.folder),
+                      title: const Text(constant.history_label),
+                    ),
+                    onPressed: () =>
+                        Navigator.of(context).pushNamed("/history")),
+              ),
+              new Card(
+                child: new FlatButton(
+                    child: new ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text(constant.settings_label),
+                    ),
+                    onPressed: () =>
+                        Navigator.of(context).pushNamed("/settings")),
+              ),
             ],
           ),
         ),
