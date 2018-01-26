4c84d6bf001f01f132d8158a6accfe14e2e17c36
Matteo Sovilla <teo.sovi@gmail.com>
2018-01-26 13:28:54 +0100

    commentato e pulito codice google_auth

--- lib/google_auth.dart
+++ lib/google_auth.dart
@@ -4,10 +4,20 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+/// Istanza globale di FirebaseAnalytics
 final analytics = new FirebaseAnalytics();
+
+/// Istanza globale di FirebaseAuth
 final firebaseAuth = FirebaseAuth.instance;
+
+/// Istanza globale di GoogleSignIn
 final googleSignIn = new GoogleSignIn();
 
+
+/// Assicura che sia stato effettuato il login.
+///
+/// Controlla se l'utente è già loggato e a seconda dei casi effettua il
+/// SignIn con account Google.
 Future<Null> ensureLoggedIn() async {
   GoogleSignInAccount user = googleSignIn.currentUser;
   if (user == null) user = await googleSignIn.signInSilently();
