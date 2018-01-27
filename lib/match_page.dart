84311fb476fe0fb70e1afcf4cf009afd35cf4855
Giulio <giulio.pace93@gmail.com>
2018-01-27 09:58:35 +0100

    reformat match_page e aggiunta funzionalità

--- lib/match_page.dart
+++ lib/match_page.dart
@@ -4,13 +4,70 @@ import 'constants.dart' as constant;
 import 'drawer.dart';
 import 'setnote_widgets.dart';
 
-class MatchPage extends StatelessWidget {
-  MatchPage({this.title});
+class MatchPage extends StatefulWidget {
+  const MatchPage({this.title});
   final String title;
+
+  @override
+  _MatchPageState createState() => new _MatchPageState(title: title);
+}
+
+class MatchData {
+  String opposingTeam = '';
+  String matchCode = '';
+  String day = '';
+  String month = '';
+  String year = '';
+  String manifestation = '';
+  String phase = '';
+  String place = '';
+  bool isMale = false;
+}
+
+class _MatchPageState extends State<MatchPage> {
+  _MatchPageState({this.title});
+
+  final String title;
+  bool _enabled = false;
+  MatchData match = new MatchData();
+
+  final TextEditingController _opposingTeamController =
+      new TextEditingController();
+  final TextEditingController _matchCodeController =
+      new TextEditingController();
+  final TextEditingController _dayController = new TextEditingController();
+  final TextEditingController _monthController = new TextEditingController();
+  final TextEditingController _yearController = new TextEditingController();
+  final TextEditingController _manifestationController =
+      new TextEditingController();
+  final TextEditingController _phaseController = new TextEditingController();
+  final TextEditingController _placeController = new TextEditingController();
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  void showInSnackBar(String value) {
+    _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text(value)));
+  }
+
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    form.save();
+    Navigator.of(context).pushNamed("/formations");
+  }
+
+  int radioValue = 0;
+
+  void handleRadioValueChanged(int value) {
+    setState(() {
+      radioValue = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+        key: _scaffoldKey,
         appBar: new AppBar(title: new Text(constant.app_name + " - " + title)),
         drawer: new Drawer(
           child: new MyDrawer(),
@@ -25,96 +82,147 @@ class MatchPage extends StatelessWidget {
                     child: new ListView(
                         padding: const EdgeInsets.symmetric(horizontal: 16.0),
                         children: <Widget>[
+                      _newOpposingTeamInput(),
+                      _newMatchCodeInput(),
                       new Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'Squadra A',
-                                labelText: 'Squadra A *',
-                              ),
-                            ),
+                            child: _newDayInput(),
                           ),
-                          const SizedBox(width: 16.0),
                           new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'Squadra B',
-                                labelText: 'Squadra B *',
-                              ),
-                            ),
+                            child: _newMonthInput(),
+                          ),
+                          new Expanded(
+                            child: _newYearInput(),
                           ),
                         ],
                       ),
-                      new Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'How do you log in?',
-                                labelText: 'New Password *',
-                              ),
-                            ),
-                          ),
-                          new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'How do you log in?',
-                                labelText: 'New Password *',
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 16.0),
-                          new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'How do you log in?',
-                                labelText: 'Re-type Password *',
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      new Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'How do you log in?',
-                                labelText: 'New Password *',
-                              ),
-                            ),
-                          ),
-                          new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'How do you log in?',
-                                labelText: 'New Password *',
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 16.0),
-                          new Expanded(
-                            child: new TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'How do you log in?',
-                                labelText: 'Re-type Password *',
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      new SetnoteButton(
-                          label: constant.formations_label,
-                          onPressed: () =>
-                              Navigator.of(context).pushNamed("/formations"))
+                      _newManifestationInput(),
+                      _newPhaseInput(),
+                      _newPlaceInput(),
+                      _newSexSwitch(),
+                      _newFormationsButton(),
                     ])),
               ),
             ],
           ),
         )));
+  }
+
+  TextFormField _newOpposingTeamInput() {
+    return new TextFormField(
+      controller: _opposingTeamController,
+      initialValue: _opposingTeamController.text,
+      decoration: const InputDecoration(
+        hintText: 'Qual è il nome della squadra avversaria?',
+        labelText: 'Squadra Avversaria *',
+      ),
+    );
+  }
+
+  TextFormField _newMatchCodeInput() {
+    return new TextFormField(
+      controller: _matchCodeController,
+      initialValue: _matchCodeController.text,
+      decoration: const InputDecoration(
+        hintText: 'Qual è il codice della gara?',
+        labelText: 'Codice Gara *',
+      ),
+    );
+  }
+
+  TextFormField _newDayInput() {
+    return new TextFormField(
+      controller: _dayController,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        hintText: 'GG',
+        labelText: 'Giorno *',
+      ),
+    );
+  }
+
+  TextFormField _newMonthInput() {
+    return new TextFormField(
+      controller: _monthController,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        hintText: 'MM',
+        labelText: 'Mese *',
+      ),
+    );
+  }
+
+  TextFormField _newYearInput() {
+    return new TextFormField(
+      controller: _yearController,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        hintText: 'AAAA',
+        labelText: 'Anno *',
+      ),
+    );
+  }
+
+  TextFormField _newManifestationInput() {
+    return new TextFormField(
+      controller: _manifestationController,
+      decoration: const InputDecoration(
+        hintText: 'Che manifestazione è?',
+        labelText: 'Manifestazione *',
+      ),
+    );
+  }
+
+  TextFormField _newPhaseInput() {
+    return new TextFormField(
+      controller: _phaseController,
+      decoration: const InputDecoration(
+        hintText: 'Che fase della competizione è?',
+        labelText: 'Fase *',
+      ),
+    );
+  }
+
+  TextFormField _newPlaceInput() {
+    return new TextFormField(
+      controller: _placeController,
+      decoration: const InputDecoration(
+        hintText: 'Dove si svolge la partita?',
+        labelText: 'Luogo *',
+      ),
+    );
+  }
+
+  Row _newSexSwitch() {
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        new Switch(
+          value: _enabled,
+          activeColor: Colors.blue,
+          inactiveThumbColor: Colors.pink,
+          inactiveTrackColor: Colors.pink[200],
+          onChanged: (bool value) {
+            setState(() {
+              _enabled = value;
+            });
+          },
+        ),
+        new Center(
+            child: new Text(
+          'asdasd',
+          textAlign: TextAlign.center,
+        )),
+      ],
+    );
+  }
+
+  SetnoteButton _newFormationsButton() {
+    return new SetnoteButton(
+      label: constant.formations_label,
+      onPressed: _handleSubmitted,
+    );
   }
 }
